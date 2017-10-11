57d06c2aa673a4c5a93296c193490852c8dd837b
Pekka Vuorela <pekka.vuorela@jollamobile.com>
2017-10-11 18:32:00 +0300

    sfos: Partly revert "trivial: Remove some unused functions" on pk_dbus_get_pid
    Need pk_dbus_get_pid for whitelisting

--- src/pk-dbus.c
+++ src/pk-dbus.c
@@ -97,7 +97,7 @@ pk_dbus_get_uid (PkDbus *dbus, const gchar *sender)
  *
  * Return value: the PID, or %G_MAXUINT if it could not be obtained
  **/
-static guint
+guint
 pk_dbus_get_pid (PkDbus *dbus, const gchar *sender)
 {
 	guint pid = G_MAXUINT;
